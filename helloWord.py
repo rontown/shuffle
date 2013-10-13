9c1249d3fc064a4f3a87248c802929e993fa918d
justingd <j.grosdesir@gmail.com>
2013-10-13 13:18:51 +0100

    First commit test
    Github with Windows

---
+++ helloWord.py
@@ -0,0 +1,10 @@
+# -*-coding:UTF-8 -*
+import os # Import os module (deal with the current operating system)
+
+
+def firstFunction():
+	print("Hello World!")
+	os.system("pause")
+
+# call the function after it's defined
+firstFunction()
